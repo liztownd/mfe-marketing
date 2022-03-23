3404cb703e797100799409d093fcbc1b49b64041
Liz Townsend <elizabeth.townsend@torch.ai>
2022-03-23 14:07:43 -0500

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Mount fn to start app
+
+const mount = (el) => {
+    ReactDOM.render(
+        <App />,
+        el
+    )
+}
+
+// cases : development & isolation all mount immediately
+if (process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if (devRoot){
+        mount(devRoot);
+    };
+};
+
+
+// else, run through container, export Mount fn
+
+export { mount };
