ad6dffec65301834866dfffd68b564ffc509f3a6
Liz Townsend <elizabeth.townsend@torch.ai>
2022-03-30 15:44:01 -0500

    update routing

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,8 +5,10 @@ import App from "./App";
 
 // Mount fn to start app
 
-const mount = (el, { onNavigate, defaultHistory }) => {
-    const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    });
 
     if (onNavigate) {
         history.listen(onNavigate);
